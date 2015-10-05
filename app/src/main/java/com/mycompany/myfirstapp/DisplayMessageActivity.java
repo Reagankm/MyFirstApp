a8fe0d52866aa113ddc5b6c4a55a75e4dbf78588
Reagan Middlebrook <reagankm@uw.edu>
2015-10-05 09:49:05 -0700

    Initial commit

--- app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/DisplayMessageActivity.java
@@ -1,23 +1,31 @@
 package com.mycompany.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_display_message, menu);
-        return true;
+        //Get message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        //Create text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Set it as the activity layout
+        setContentView(textView);
+
+
     }
 
     @Override
